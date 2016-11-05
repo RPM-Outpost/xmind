0fa48bb0c1eff3643b9dacfe7c4977e67bc8e24c
TheElectronWill <theelectronwill@gmail.com>
2016-11-05 15:36:54 +0100

    Commit work

---
+++ create-package.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+# Author: TheElectronWill
+# This script downloads the latest version of XMind for linux, and creates a package with rpmbuild.
+
+rpm_dir=$PWD/RPMs
+desktop_model=$PWD/xmind.desktop
+spec_file=$PWD/xmind.spec
+icon_file=$PWD/xmind-logo.png
+
+work_dir=$PWD/work
+downloaded_dir=$work_dir/xmind
+desktop_file=$work_dir/xmind.desktop
+
+# Download the xmind zip archive.
+function download_xmind {
+	echo 'Downloading xmind for linux. This may take a while...'
+	wget -q --show-progress "http://dl2.xmind.net/xmind-downloads/xmind-8-linux.zip"
+}
+
+# Asks the user if he/she wants to remove the specified directory, and removes it if he wants to.
+function ask_remove_dir {
+	read -p "Do you want to remove the \"$1\" directory? [y/N]" answer
+	case $answer in
+		[Yy]* )
+			rm -r "$1"
+			echo "\"$1\" directory removed."		
+			;;
+		* ) echo "Ok, I won't remove it." ;;
+	esac
+}
+
+# If the specified directory exists, asks the user if he/she wants to remove it.
+# If it doesn't exist, creates it.
+function manage_dir {
+	if [ -d "$1" ]; then
+		echo "The $2 directory already exist. It may contain outdated things."
+		ask_remove_dir "$1"
+	fi
+	mkdir -p "$work_dir"
+}
+
+manage_dir "$work_dir" 'work'
+manage_dir "$rpm_dir" 'RPMs'
+cd "$work_dir"
+
+# Download xmind if needed
+archive_name='xmind-8-linux.zip'
+if [ -e "$archive_name" ]; then
+	echo "Found $archive_name"
+	read -p 'Do you want to use this archive instead of downloading a new one? [y/N]' answer
+	case $answer in
+		[Yy]* )
+			echo 'Ok, I will use this this archive.'
+			;;
+		* )
+			download_xmind
+			;;
+	esac
+else
+	download_xmind
+fi
+
+# Extracts the archive
+echo 'Extracting the files...'
+if [ ! -d "$downloaded_dir" ]; then
+	mkdir "$downloaded_dir"
+fi
+unzip -q "$archive_name" -d "$downloaded_dir"
+
+# Gets infos
+echo 'Analysing the files...'
+arch=$(uname -m)
+if [ "$arch" == "x86_64" ]; then
+	executable="$downloaded_dir/XMind_amd64/XMind"
+	executable_dir="XMind_amd64"
+else
+	executable="$downloaded_dir/XMind_i386/XMind"
+	executable_dir="XMind_i386"
+	arch="i386"
+fi
+echo "Archive: $archive_name"
+echo "Architecture: $arch"
+echo "Executable: $executable"
+
+# Creates a .desktop file:
+echo 'Creating .desktop file...'
+cp "$icon_file" "$downloaded_dir"
+sed "s/_dir/$executable_dir/" "$desktop_model" > "$desktop_file"
+
+# Fixes XMind.ini:
+echo 'Fixing XMind.ini...'
+ini_file="$downloaded_dir/$executable_dir/XMind.ini"
+user_xmind_config="@user.home/.config/xmind/configuration"
+user_xmind_workspace="@user.home/.config/xmind/workspace"
+sed -i "s|./configuration|$user_xmind_config|; s|../workspace|$user_xmind_workspace|" "$ini_file"
+
+# Chooses the spec file based on the system's architecture and build the packages
+echo 'Creating the RPM package...'
+rpmbuild -bb --nocheck $spec_file --define "_topdir $work_dir" --define "_rpmdir $rpm_dir" --define "arch $arch" --define "downloaded_dir $downloaded_dir" --define "desktop_file $desktop_file"
+
+echo '-----------'
+echo 'Done!'
+echo "The RPM package is located in the \"RPMs/$arch\" folder."
+
+# Removes the work directory if the user wants to
+ask_remove_dir "$work_dir"
---
+++ xmind.spec
@@ -0,0 +1,42 @@
+# Defined by the caller (ie the script):
+# downloaded_dir
+# desktop_file
+# arch
+
+%define install_dir /opt/xmind-8
+%define apps_dir /usr/share/applications
+
+# Disable brp-java-repack-jars which is really slow, and not useful for XMind.
+%define __jar_repack 0
+
+Name:		xmind
+Version:	8
+Release:	1%{?dist}
+Summary:	Free Voice and Text Chat for Gamers.
+
+Group:		Applications/Office
+License:	EPL, LGPLv3
+URL:		https://www.xmind.net/
+BuildArch:	%{arch}
+Requires:   java, gtk3
+
+# Disable automatic dependency processing, so that the rpm package only
+# provides xmind, and not crazy OSGI stuff.
+AutoReqProv: no
+
+%description
+Mind Mapping Software
+
+%prep
+
+%build
+
+%install
+mkdir -p "%{buildroot}%{install_dir}"
+mkdir -p "%{buildroot}%{apps_dir}"
+mv "%{downloaded_dir}"/* "%{buildroot}%{install_dir}"
+cp "%{desktop_file}" "%{buildroot}%{apps_dir}"
+
+%files
+/*
+
