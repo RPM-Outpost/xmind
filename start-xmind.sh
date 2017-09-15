d691740732310ed02242ad82b874eebf65b8c242
TheElectronWill <theelectronwill@gmail.com>
2017-09-15 22:58:56 +0200

    Ability to open files with XMind

--- start-xmind.sh
+++ start-xmind.sh
@@ -4,4 +4,4 @@ if [ ! -d "$config_dir" ]; then
 	mkdir -p "$config_dir"
 	cp -ar /opt/xmind-8/@dir/configuration/* "$config_dir"
 fi
-exec /opt/xmind-8/@dir/XMind -configuration $config_dir -data $config_dir "$@"
+exec /opt/xmind-8/@dir/XMind "$@"
